27d35831ce454680b4fb7ff456bb5975c222b9d0
Ananta Tiwari <tiwari@cs.umd.edu>
2010-11-02 21:50:20 +0000

    kill the child processes

--- code-server/build/cutil.c
+++ code-server/build/cutil.c
@@ -5,7 +5,10 @@
  *
  ***/
 #include "cutil.h"
+#ifdef linux
+#include <sys/prctl.h>
 #include <signal.h>
+#endif
 
 /***
  *
@@ -15,7 +18,7 @@
 void h_exit(char *errmesg){
   perror(errmesg);
 #ifdef linux
-	prctl(PR_SET_PDEATHSIG, SIGHUP);
+  prctl(PR_SET_PDEATHSIG, SIGHUP);
 #endif
   exit(1);
 }
