b71a5dee3c8aa32e836a0846266d9d230fb9f9fa
Ray Chen <rchen@cs.umd.edu>
2016-02-25 00:40:06 -0500

    Fix STRICT=1 compilation on OS X with clang.
    Verified with Command Line Tools (OS X 10.10) for Xcode: v7.2

--- example/client_api/multi.c
+++ example/client_api/multi.c
@@ -16,7 +16,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with Active Harmony.  If not, see <http://www.gnu.org/licenses/>.
  */
-#define _XOPEN_SOURCE 500 // Needed for lrand48().
+#define _XOPEN_SOURCE 600 // Needed for lrand48() and snprintf().
 
 #include <stdio.h>
 #include <stdlib.h>
